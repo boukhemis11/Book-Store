2fa5a5e8589fd4acbe936758c597b026edd3e670
Boukhemis Dib <boukhemis2011@gmail.com>
2020-05-03 14:18:10 +0200

    configuration app.prop

---
+++ booktstore/src/test/java/dib/dev/bookstore/booktstore/BooktstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dib.dev.bookstore.booktstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
