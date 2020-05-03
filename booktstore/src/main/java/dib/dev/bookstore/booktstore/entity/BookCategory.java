1b8817594bca7f154ce2c94290f40f7dd8b9efe6
Boukhemis Dib <boukhemis2011@gmail.com>
2020-05-03 14:31:40 +0200

    add entity book & bookcategory

---
+++ booktstore/src/main/java/dib/dev/bookstore/booktstore/entity/BookCategory.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package dib.dev.bookstore.booktstore.entity;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author boukh
+ *
+ */
+
+@Entity
+@Table
+@Setter
+@Getter
+@ToString
+public class BookCategory {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="category_name")
+	private String categoryName;
+	
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="category")
+	private Set<Book> book;
+}
